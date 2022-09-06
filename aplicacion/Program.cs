daf89c8e81be1287c1eee19409970f20e20ad3ff
Andr3y11 <93442567+Andr3y11@users.noreply.github.com>
2022-09-05 20:52:32 -0500

    applicacion

--- aplicacion/Program.cs
+++ aplicacion/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using persistencia;
+using dominio;
 
 namespace aplicacion
 {
     class Program
     {
+        private static IRepositorio_Personas _repo=new Repositorio_Personas(new ApplicationContext());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona p1 =new Persona();
+            p1.Nombre="Camila";
+            _repo.Add(p1);
+            //_repo.Update(p1);
+            //_repo.Delete(3);
+            //_repo.GetAll();
+            
+
+
+
         }
     }
-    
 }
