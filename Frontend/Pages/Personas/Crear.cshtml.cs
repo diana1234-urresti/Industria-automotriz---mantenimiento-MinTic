00b1f6e27b7ef0929cb91670d132e975c7665b0f
fsolarte13 <fabersolarte29@hotmail.com>
2022-09-08 13:24:12 -0500

    Crear_personas_listo

--- Frontend/Pages/Personas/Crear.cshtml.cs
+++ Frontend/Pages/Personas/Crear.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using dominio;
+using persistencia;
 
 namespace MyApp.Namespace
 {
     public class CrearModel : PageModel
     {
+        private readonly IRepositorio_Personas _repo_personas;
+        public Persona Personas { get; set; }
+
+         public CrearModel(IRepositorio_Personas Repositorio_Personas)
+           {
+            _repo_personas = Repositorio_Personas;
+            
+           }
+ 
+
         public void OnGet()
         {
+        }
+
+         public IActionResult OnPost(Persona Personas)
+        {
+            _repo_personas.Add(Personas);
+            return new RedirectToPageResult("/Personas/listar");
         }
     }
 }
