6b0748a77f98310da1770a8c71da7b4897d8fc10
Andr3y11 <93442567+Andr3y11@users.noreply.github.com>
2022-09-07 21:09:34 -0500

    REPOSITORIO FACT

---
+++ persistencia/IRepositorio_Factura.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dominio;
+
+namespace persistencia
+{
+    public interface IRepositorio_Factura
+    {
+        Factura Add(Factura facturas);
+        IEnumerable<Factura> GetAll();
+        Factura Update (Factura facturas);
+        void Delete (int Id);
+        Factura Get(Factura facturas); 
+    }
+}
