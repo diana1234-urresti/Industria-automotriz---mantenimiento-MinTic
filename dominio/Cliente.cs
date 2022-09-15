7213d60c9c93718bcf5613c885aa553e067d9f92
fsolarte13 <fabersolarte29@hotmail.com>
2022-09-14 21:46:54 -0500

    vehiculo

--- dominio/Cliente.cs
+++ dominio/Cliente.cs
@@ -1,20 +1,24 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace dominio
 {
     public class Cliente
-    {
-        public int Id {get;set;}
+    {   
+        
+        [Key]
+        public int Id_cliente {get;set;}
+
         [Required(ErrorMessage = "Ingrese Ciudad")]
         [StringLength(50)]
         public string Ciudad {get;set;}
+
         [Required(ErrorMessage = "Fecha de Ingreso Obligatorio")]
-        [StringLength(50)]
         public DateTime FechaIngreso {get;set;}
-        [Required(ErrorMessage = "Ingrese Tipo de Vehiculo")]
-        [StringLength(50)]
-        public string TipoVehiculo {get;set;}
+
+        public virtual Vehiculo Vehiculo { get; set;}
         
     }
 }
